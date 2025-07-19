22ab04c3a86375b3bb1a56093d143628a6733e7a
yosef langer <yosef@doar.systems>
2025-07-20 04:57:00 +0900

    updates

--- src/EditorPreviewWithDialog.js
+++ src/EditorPreviewWithDialog.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import EditorDialog from "./EditorDialog";
 import EmailPreview from "./EditorPreview";
 
@@ -9,24 +9,35 @@ const EditorPreviewWithDialog = (props) => {
         html: (props.editorCommunication.data && props.editorCommunication.data.html) || '',
         values: props.editorCommunication.data && props.editorCommunication.data.values
     });
+
     const onChangeHandler = useCallback((data) => {
         if (typeof props.onChange === 'function') {
             props.onChange(data);
         }
         setData(data);
     }, [props.onChange, setData]);
+
+    useEffect(() => {
+        if (!data) {
+            onChangeHandler({
+                html: (props.editorCommunication.data && props.editorCommunication.data.html) || '',
+                values: props.editorCommunication.data && props.editorCommunication.data.values
+            });
+        }
+    }, [data, onChangeHandler, props.editorCommunication.data]);
+
     return (
         <>
-            <EmailPreview html={data.html}
-                          subject={data.values.subject}
-                          subTitle={data.values.subTitle}
+            <EmailPreview html={data?.html}
+                          subject={data?.values?.subject}
+                          subTitle={data?.values?.subTitle}
                           onClick={() => setOpen(true)}
             />
             <EditorDialog {...props}
                           open={open}
                           onClose={() => setOpen(false)}
                           onChange={onChangeHandler}
-                          initialValues={data.values}
+                          initialValues={data?.values}
             />
         </>
 
